99dbc44ad98fe52411c32c33cad01a2a335ad853
Rohidul Islam <rohidul209@gmail.com>
2021-12-22 04:07:49 +0600

    Initial Commit

---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Rohid on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
